62736e0d21119e4c1a5a0e9081349bb97f12207c
Ahmed Osama <32176872+ahmedosama07@users.noreply.github.com>
2023-12-29 22:45:03 +0200

    CDC IO build

---
+++ mega48/default/dep/usbdrvasm.o.d
@@ -0,0 +1,37 @@
+usbdrvasm.o: ../../usbdrv/usbdrvasm.S ../../usbdrv/usbportability.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/io.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/sfr_defs.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/iom8.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/portpins.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/common.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/version.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/fuse.h \
+ /opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/lock.h \
+ ../../usbdrv/usbdrv.h ../usbconfig.h ../../usbdrv/usbdrvasm12.inc \
+ ../../usbdrv/asmcommon.inc
+
+../../usbdrv/usbportability.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/io.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/sfr_defs.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/iom8.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/portpins.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/common.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/version.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/fuse.h:
+
+/opt/avr8-gnu-toolchain-linux_x86_64/avr/include/avr/lock.h:
+
+../../usbdrv/usbdrv.h:
+
+../usbconfig.h:
+
+../../usbdrv/usbdrvasm12.inc:
+
+../../usbdrv/asmcommon.inc:
